3652e8f50e6fb0af0f17fbfeea18a9a7a1a31c63
iboy15 <iboy_ishak@yahoo.com>
2018-11-06 20:46:58 +0700

    my first commit

---
+++ src/components/actions/members.js
@@ -0,0 +1,31 @@
+import axios from 'axios'
+
+export const fetchMember  = () => {
+    return (dispatch) => {
+        axios.get('https://reqres.in/api/users?page=2')
+            .then(response => {
+                const members = response.data
+                dispatch({
+                    type : 'ADD_MEMBERS',
+                    payload : {
+                        members : members.data
+                    }
+                })
+            })
+    }
+}
+
+export const fetchEvent  = () => {
+    return (dispatch) => {
+        axios.get('https://swapi.co/api/films/')
+            .then(response => {
+                const events = response.data
+                dispatch({
+                    type : 'ADD_EVENTS',
+                    payload : {
+                        events : events.results
+                    }
+                })
+            })
+    }
+}
